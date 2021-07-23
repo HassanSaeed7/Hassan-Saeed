7d9ba0627ff82702f57769aa57fd08fb7599a27d
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-07-22 22:09:53 -0700

    animation

--- MyWebsite/typer.js
+++ MyWebsite/typer.js
@@ -1,7 +1,7 @@
 let text = "I am Hassan Saeed.";
 let i = 0;
 setInterval(function() {
-    let cursor = i%4==0 || i%3==0 ? "_" : "&nbsp;")
+    let cursor = (i%4==0 || i%3==0 ? "_" : "&nbsp;")
     let displayText = text.substring(0, i % text.length);
     document.getElementById("typespace").innerHTML = displayText;
     i++;
