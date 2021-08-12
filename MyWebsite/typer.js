cca61345306ffb619ac8e1bd8826671d6a2edac7
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-08-11 21:46:20 -0700

    1

--- MyWebsite/typer.js
+++
@@ -1,8 +0,0 @@
-let text = "Hello, I am Hassan Saeed.";
-let i = 0;
-setInterval(function() {
-    let cursor = (i%4==0 || i%3==0 ? "_" : "&nbsp;")
-    let displayText = text.substring(0, i % text.length);
-    document.getElementById("typespace").innerHTML = displayText;
-    i++;
-}, 500);
