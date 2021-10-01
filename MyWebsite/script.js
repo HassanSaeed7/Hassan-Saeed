74a3bb6e3e3096a878b8bef7925605177ebc7b66
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-10-01 01:40:23 -0700

    g

--- MyWebsite/script.js
+++ MyWebsite/script.js
@@ -1,6 +1,19 @@
-const parallax = document.getElementById("parallax")
+// const parallax = document.getElementById("parallax")
 
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset
-    parallax.style.backgroundPositionY = offset * 1.5 + "px"
-})
+// window.addEventListener("scroll", function() {
+//     let offset = window.pageYOffset
+//     parallax.style.backgroundPositionY = offset * 1.5 + "px"
+// })
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
