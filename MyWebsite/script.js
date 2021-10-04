b4e87c10ad27dbf3ec652d467f39efc8e40f90ee
HassanSaeed7 <Hsaeed1007@gmail.com>
2021-10-04 01:13:24 -0700

    projects styling

--- MyWebsite/script.js
+++ MyWebsite/script.js
@@ -1,9 +1,9 @@
-const parallax = document.getElementById("parallax")
+// const parallax = document.getElementById("parallax")
 
-window.addEventListener("scroll", function() {
-    let offset = window.pageYOffset
-    parallax.style.backgroundPositionY = offset * 0.7 + "px"
-})
+// window.addEventListener("scroll", function() {
+//     let offset = window.pageYOffset
+//     parallax.style.backgroundPositionY = offset * 0.7 + "px"
+// })
 
 
 
