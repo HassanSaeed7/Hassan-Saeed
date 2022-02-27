53bd2844ff649894d1263200f4cbc204194ba85f
HassanSaeed7 <Hsaeed1007@gmail.com>
2022-02-26 17:16:53 -0800

    branch

--- script.js
+++
@@ -1,8 +0,0 @@
-function toggleClass(element) {
-    if(element.className == "project-img") {
-        element.Classname = "blurb show"
-    } else {
-        element.className = "project-img"
-    }
-    
-}
